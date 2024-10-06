d976f5fd5974745eae44912a7fdc6db83a7d4477
Cohnny Flobrandt <119353835+Cohnny@users.noreply.github.com>
2024-10-06 15:26:09 +0200

    Updated source file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import random
 from datetime import datetime
 
 
-def get_player_name():
+def set_player_name():
+    """ Asks user to enter full name, validates the input and makes sure that there is a whitespace between them. """
     formatted_name = ""
     run = True
 
@@ -18,7 +20,8 @@ def get_player_name():
     return formatted_name
 
 
-def get_player_birthdate():
+def set_player_birthdate():
+    """ Asks user to enter brith date, validates input and then returns it """
     player_birthdate = 0
     run = True
 
@@ -30,7 +33,6 @@ def get_player_birthdate():
             month = int(birthdate[4:6])
             day = int(birthdate[6:8])
 
-            # Validate year, month, and day
             if year > 1900 and 1 <= month <= 12 and 1 <= day <= 31:
                 player_birthdate = f"{year:04d}-{month:02d}-{day:02d}"
                 run = False
@@ -43,6 +45,7 @@ def get_player_birthdate():
 
 
 def calculate_player_age(player_birthdate):
+    """" Takes the player_birthdate and calculates the age of the player """
     birthdate = datetime.strptime(player_birthdate, "%Y-%m-%d")
     today = datetime.today()
     player_age = today.year - birthdate.year
@@ -53,14 +56,49 @@ def calculate_player_age(player_birthdate):
     return player_age
 
 
+def get_random_integer():
+    """ Randomly generates a random integer from 0-100 """
+    random_int = random.randint(0, 100)
+
+    return random_int
+
+
+def create_lucky_list():
+    """ Creates a list of lucky numbers """
+    lucky_list = []
+    for i in range(9):
+        random_number = get_random_integer()
+        lucky_list.append(random_number)
+
+    return lucky_list
+
+
+def print_lucky_list(lucky_list):
+    """ Prints the lucky numbers """
+    random.shuffle(lucky_list)
+    lucky_numbers = "Your lucky numbers are: "
+
+    for num in lucky_list:
+        lucky_numbers += str(num) + ", "
+
+    lucky_numbers = lucky_numbers.rstrip(", ")
+
+    print(lucky_numbers)
+
+
 def main():
-    player_name = get_player_name()
-    player_birthdate = get_player_birthdate()
+    player_name = set_player_name()
+    player_birthdate = set_player_birthdate()
     player_age = calculate_player_age(player_birthdate)
     while player_age < 18:
         print(f"You need to be at least 18 years old to play. You entered that you are {player_age} years old.")
-        player_birthdate = get_player_birthdate()
+        player_birthdate = set_player_birthdate()
         player_age = calculate_player_age(player_birthdate)
+    lucky_list = create_lucky_list()
+    lucky_number = get_random_integer()
+    lucky_list.append(lucky_number)
+    print_lucky_list(lucky_list)
+
 
 
 main()
